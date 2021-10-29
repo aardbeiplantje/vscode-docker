1d5a247592f049ae6eb3d4c356d4905e7e1dd9f9
Tim Aerts <aardbeiplantje@gmail.com>
2021-10-29 12:37:08 +0200

    runvscode.sh: add very simple ~/.vscode-docker cfg dir capabilities

--- runvscode.sh
+++ runvscode.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+VSCODE_OAUTH2_CONFIG=${VSCODE_OAUTH2_CONFIG:-~/.vscode-docker/vscode-${WHAT:-github}.sh}
+if [ -f "$VSCODE_OAUTH2_CONFIG" ]; then
+    . "$VSCODE_OAUTH2_CONFIG"
+fi
+
 USER=${USER:-openvscode-server}
 HOME=${HOME:-/home/$USER}
 DOCKER_IMAGE_NAME=${DOCKER_IMAGE_NAME:-vscode/workspace}
-DOCKER_TAG=latest
+DOCKER_TAG=${DOCKER_TAG:-latest}
 DOCKER_REPOSITORY=${DOCKER_REPOSITORY:-images_$USER}
 DOCKER_IMAGE="$DOCKER_REPOSITORY/$DOCKER_IMAGE_NAME:$DOCKER_TAG"
-VSCODE_HOME=/home/workspace
+VSCODE_HOME=${VSCODE_HOME:-/home/workspace}
 
 docker build \
     -t $DOCKER_IMAGE \
